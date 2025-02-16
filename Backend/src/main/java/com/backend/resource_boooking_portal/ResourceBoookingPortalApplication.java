ffe7469dbd63e0d930f8c57a8ac74bd54409a34f
Kavin778 <kavin.m6578@gmail.com>
2025-02-16 17:03:38 +0530

    Creating backend folder

---
+++ Backend/src/main/java/com/backend/resource_boooking_portal/ResourceBoookingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.backend.resource_boooking_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceBoookingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceBoookingPortalApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/backend/resource_boooking_portal/ResourceBoookingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.resource_boooking_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceBoookingPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
